65338c09a1d3bc0120f910bfb5e3f672db2b7af1
Odogiyon01 <66860639+Odogiyon01@users.noreply.github.com>
2025-09-04 23:30:58 +0100

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const admin = require("firebase-admin");
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_KEY)),
+  });
+}
+
+const db = admin.firestore();
+module.exports = { admin, db };
